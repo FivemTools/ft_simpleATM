7c25009072b3c60681969ee7390546dc0a96d6f9
CanadienFr <provenchja2@msn.com>
2017-07-10 18:41:03 -0400

    Update 0.2.2

--- cl_init.lua
+++ cl_init.lua
@@ -7,14 +7,14 @@ Citizen.CreateThread(function()
         generateButton()
         
         if config.showBlips then
-          for _, pos in pairs (config.locationATM) do
-            exports.ft_blips:Add("simpleATMBlips"..tostring(i), {text = translation[language].title, colorId = 2, imageId = 277, x = pos[1], y = pos[2], z = pos[3] })
+          for k, pos in pairs (config.locationATM) do
+            exports.ft_blips:Add("simpleATMBlips"..tostring(k), {text = translation[language].title, colorId = 2, imageId = 277, x = pos[1], y = pos[2], z = pos[3] })
           end
         end
         
-        for _, pos in pairs (config.locationATM) do
-          exports.ft_markers:Add("simpleATMMarkers"..tostring(i), { x = pos[1], y = pos[2], z = pos[3], type = 1, weight = 1, height = 0.5, red = config.menu.red, green = config.menu.green, blue = config.menu.blue, enable = true })
-          exports.ft_areas:Add("simpleATMAreas"..tostring(i), {x = pos[1], y = pos[2], z = pos[3] , weight = 2, active = { callback = enterATM }, exit = { callback = exitATM }, enable = true })
+        for k, pos in pairs (config.locationATM) do
+          exports.ft_markers:Add("simpleATMMarkers"..tostring(k), { x = pos[1], y = pos[2], z = pos[3], type = 1, weight = 1, height = 0.5, red = config.menu.red, green = config.menu.green, blue = config.menu.blue, enable = true })
+          exports.ft_areas:Add("simpleATMAreas"..tostring(k), {x = pos[1], y = pos[2], z = pos[3] , weight = 2, active = { callback = enterATM }, exit = { callback = exitATM }, enable = true })
         end
         
       end
